1b07483f5dff3b3a9cb8673329d0d3524499b254
iaraloayza <anaiaraloayza11@gmail.com>
2025-07-05 20:54:51 -0300

    refactor: :memo: renomeação da contact controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CreateContactController;
+use App\Http\Controllers\ContactController;
 
 Route::get('/', function () {
     return redirect()->route('contacts.index');
 });
 
 // Rotas Contatos
-Route::resource('contacts', CreateContactController::class)->only(['index', 'store', 'update', 'destroy']);
+Route::resource('contacts', ContactController::class)->only(['index', 'store', 'update', 'destroy']);
